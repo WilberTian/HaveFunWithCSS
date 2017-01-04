364f50b1821087dd42fa24fcd951a19426e8606c
WilberTian <tianli917@hotmail.com>
2017-01-04 20:51:07 +0800

    update vm.js to cache the view and clear zombie view

--- client/app/vm.js
+++ client/app/vm.js
@@ -3,14 +3,18 @@ define([
     'underscore',
     'backbone'
 ], function($, _, Backbone){
+
     var cachedViews = {};
 
     var create = function (key, View, options, useCachedView) {
         if(key in cachedViews) {
             if(useCachedView) return cachedViews[key];
 
-            cachedViews[key].undelegateEvents();
-            cachedViews[key].remove();
+            if('dispose' in cachedViews[key]) {
+                cachedViews[key].dispose();
+            } else {
+                throw 'should define a `dispose` method in none cache mode'
+            }
         } 
 
         var view = new View(options);
